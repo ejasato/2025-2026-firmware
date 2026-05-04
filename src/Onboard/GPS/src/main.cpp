54a2d969aad76f4dfa3e214ee14ad1f20ac45e3b
ejasato <112989883+ejasato@users.noreply.github.com>
2026-05-03 18:22:56 -0700

    GPSOnboard

--- src/Onboard/GPS/src/main.cpp
+++ src/Onboard/GPS/src/main.cpp
@@ -3,38 +3,49 @@
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 #include "CANTProtocol.h"
 
-CANTProtocol CAN(10,2,0xA); //need to check pins
+CANTProtocol CAN(5,17,0xA); //need to check pins
 SFE_UBLOX_GNSS myGNSS;
+
 typedef struct GPSPosition{
     float longitude;
     float latitude;
     float atltitude;
 }GPSPosition;
 
-void builder(unsigned char dataLength, byte* incomingData, unsigned long callbackID){
+void builderP(unsigned char dataLength, byte* incomingData, unsigned long callbackID){
     GPSPosition longitude = myGNSS.getLongitude();
     GPSPosition latitude = myGNSS.getLatitude();
     GPSPosition altitude = myGNSS.getAltitude();
-    uint32_t time = myGNSS.getUnixEpoch();
-    CAN.registerRequestReponse(GPSPosition, callbackID);
-    CAN.registerRequestReponse(time, callbackID);
+    CAN.sendRequestReponse(GPSPosition, callbackID);
 }
 
-void builder1(unsigned char dataLength, byte* incomingData, unsigned long callbackID){
+void builderT(unsigned char dataLength, byte* incomingData, unsigned long callbackID){
     uint32_t time = myGNSS.getUnixEpoch();
-    CAN.registerRequestReponse(time, callbackID);
+    CAN.snedRequestReponse(time, callbackID);
+}
+
+void builderR(unsigned char dataLength, byte* incomingData, unsigned long callbackID){
+    CAN.sendRequestReponse(Boolean true, callbackID);
 }
 
 void setup()
 {
+    while(!CAN.begin()){
+      delay(100);
+    }
     Wire.begin();
-  myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
-  myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); //Save (only) the communications port settings to flash and BBR
-  CAN.registerRequest(0, builder);
-  CAN.registerRequest(1, builder1);
-  while(!CAN.begin()){
-    delay(100);
-  }
+    while(!myGNSS.begin()){ 
+        delay(100);
+    }
+    myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)  
+    myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); //Save (only) the communications port settings to flash and BBR
+    CAN.registerRequest(0, builderP);
+    CAN.registerRequest(1, builderT);
+    uint8_t Ready = myGNSS.getFixType();
+    while(myGNSS.getFixType() < 3){  // keep checking until good fix
+        delay(100);
+    }
+    CAN.registerRequest(2, builderR);
 }
 
 void loop(){
